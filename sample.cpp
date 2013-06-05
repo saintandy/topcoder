232c993ac708855b6f5cc5e974bd155b9c7e76b7
vlad <vladtarniceru@yahoo.com>
2013-06-06 02:01:23 +0300

    added sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <map>
+#include <set>
+using namespace std;
+
+#define INF 2100000000
+#define PI 3.14159265358979323
+#define eps 0.0000001
+
+typedef long double lod;
+typedef long long lol;
+
+
+
+class <%:class-name%> {
+  public:
+	inline <%:return-type%> <%:method-name%>(<%:param-list%>) {
+		
+	}
+};
+
+
+
+
+
+
